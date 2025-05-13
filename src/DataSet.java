6e16519c282a4f40486c85d32673122ce790a2b3
Haos <dvorkin.alon@gmail.com>
2025-05-13 11:15:42 +0300

    DataSet first part comments

--- src/DataSet.java
+++ src/DataSet.java
@@ -1,23 +1,43 @@
 public class DataSet {
 
+    /** array of information*/
     private String[] data;
+    /** length of data */
     private int size;
+    /** number of items in data which aren't nulls */
     private int activeItems;
+    /** errors value */
     private final int ERROR = -1;
 
+    /**
+     * Class Builder
+     * @param size length of {@code data}
+     */
     public DataSet( int size ){
         this.size = size;
         data = new String[ this.size ];
     }
 
+    /**
+     * Checks if {@code data} is empty
+     * @return 1 if {@code data} is empty, otherwise 0
+     */
     public boolean isEmpty(){
-        return activeItems > 0;
+        return activeItems == 0;
     }
 
+    /**
+     * Checks if {@code data} is full
+     * <p>Note: {@code data}  is full if all items inside aren't nulls</p>
+     * @return 1 if {@code data} is full
+     */
     public boolean isFull(){
         return activeItems == size;
     }
 
+    /**
+
+     */
     public void printItemByIndex( int index ){
         System.out.print( this.data[index] );
     }
